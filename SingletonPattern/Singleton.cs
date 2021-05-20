a2449244aac57aca97bf894e943a463a9413a988
年俊 黄 <765809427@qq.com>
2021-05-20 14:09:19 +0800

    2021/5/20 单例模式

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private volatile static Singleton uniqueInstance;
+
+        public static readonly object SingletonLocker = new object();
+
+        private Singleton() { }
+
+        public static Singleton getInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                lock (SingletonLocker)
+                {
+                    if (uniqueInstance == null)
+                        uniqueInstance = new Singleton();
+                }
+            }
+            return uniqueInstance;
+        }
+    }
+}
